3781c706e4878eb90a456ac6c926d8f1667a4790
Mugivara <115212029+bvolodymyr@users.noreply.github.com>
2024-03-13 18:12:36 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/TosterProvider";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify",
+  description: "Listen to musicnpm",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider/>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+              <Sidebar>
+                {children}
+              </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+        </body>
+    </html>
+  );
+}
